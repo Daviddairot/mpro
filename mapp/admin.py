e23d56c41df400db30ede8269fcc1e10af461a13
USER <dairotemitopedavid@gmail.com>
2026-07-23 09:38:03 +0100

    update

--- mapp/admin.py
+++ mapp/admin.py
@@ -4,6 +4,10 @@ from openpyxl import Workbook
 from openpyxl.utils import get_column_letter
 from .models import Grade, Student, Assessment, CA
 
+admin.site.site_header = "Musicology Admin"
+admin.site.site_title = "Musicology Admin"
+admin.site.index_title = "Musicology Admin"
+
 @admin.action(description='Export selected assessments to Excel')
 def export_assessments_to_excel(modeladmin, request, queryset):
     wb = Workbook()
@@ -41,6 +45,36 @@ def export_assessments_to_excel(modeladmin, request, queryset):
     wb.save(response)
     return response
 
+@admin.action(description='Export selected grades to Excel')
+def export_grades_to_excel(modeladmin, request, queryset):
+    wb = Workbook()
+    ws = wb.active
+    ws.title = "Grades"
+
+    headers = ['S/N', 'Student', 'Matric Number', 'CA', 'Exam', 'Total']
+    ws.append(headers)
+
+    for idx, grade in enumerate(queryset.select_related('student', 'ca'), start=1):
+        ws.append([
+            idx,
+            f"{grade.student.first_name} {grade.student.last_name}",
+            grade.student.matric_number,
+            float(grade.ca.total) if grade.ca else 0,
+            float(grade.score),
+            float(grade.total)
+        ])
+
+    for i, header in enumerate(headers, 1):
+        ws.column_dimensions[get_column_letter(i)].width = max(10, len(header) + 2)
+
+    response = HttpResponse(
+        content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+    )
+    response['Content-Disposition'] = 'attachment; filename="grades_export.xlsx"'
+    wb.save(response)
+    return response
+
+
 class StudentAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'matric_number', 'instrument')
     search_fields = ('first_name', 'last_name', 'matric_number', 'instrument')
@@ -62,6 +96,7 @@ class GradeAdmin(admin.ModelAdmin):
     list_filter = ('student__instrument',)
     readonly_fields = ('score', 'total')
     autocomplete_fields = ('student', 'ca')
+    actions = [export_grades_to_excel]
 
 admin.site.register(Student, StudentAdmin)
 admin.site.register(Assessment, AssessmentAdmin)
